d1fd2b0e7efdf4856bfa0f8db1e000ca081dc491
Justin Wilkey <justin.wilkey@tentkeep.com>
2018-04-14 21:09:46 -0400

    Add Titus textual components

---
+++ src/components/studies/Titus/Textual/1_10-14.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="textual">
+    For there are many who are insubordinate,
+    empty talkers and deceivers,
+    especially those of the circumcision party.
+
+    They must be silenced,
+    since they are upsetting whole families
+    by teaching for shameful gain
+    what they ought not to teach.
+
+    One of the Cretans,
+    a prophet of their own,
+    said,
+    "Cretans are always liars,
+    evil beasts,
+    lazy gluttons."
+    This testimony is true.
+
+    Therefore rebuke them sharply,
+    that they may be sound in the faith,
+    not devoting themselves to Jewish myths
+    and the commands of people who turn away
+    from the truth.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Titus1_10-14',
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.textual {
+}
+</style>
---
+++ src/components/studies/Titus/Textual/1_6-9.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="textual">
+    if anyone is above reproach,
+    the husband of one wife,
+    and his children are believers
+    and not open to the charge of debauchery or insubordination.
+
+    For an overseer,
+    as God's steward,
+    must be above reproach.
+    He must not be arrogant
+    or quick-tempered
+    or a drunkard
+    or violent
+    or greedy for gain,
+
+    but hospitable,
+    a lover of good,
+    self-controlled,
+    upright,
+    holy,
+    and disciplined.
+
+    He must hold firm to the trustworthy word as taught,
+    so that he may be able to give instruction
+    in sound doctrine
+    and also to rebuke those who contradict it.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Titus1_6-9',
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.textual {
+}
+</style>
