e23b3979d75540b7e2c157f9ad3a734cd1eb2cd5
Justin Wilkey <jwilkey@Justins-MacBook-Pro.local>
2019-01-12 15:53:10 -0500

    Populate family worship

--- src/components/resources/FamilyWorship.vue
+++ src/components/resources/FamilyWorship.vue
@@ -2,10 +2,27 @@
   <div class="content">
     <div class="pad">
       <div class="theme-mid pad shadow rounded">
-        <p>Intro</p>
-        <p>Intro prayer - Hymn - Intentional prayer - Bible - discuss</p>
-        <p>Music videos - YouTube playlist</p>
-        <p>Read books together aloud</p>
+        <h3>Supplies</h3>
+        <p>Hymnal</p>
+        <p>Bible</p>
+
+        <hr>
+
+        <h3>What to do (15 minutes)</h3>
+        <p>• Everyone sits together. No toys. Turn off TV, radio, distractions. No devices.</p>
+        <p>• Intro prayer ("...God, help us to know you... be pleased with our worship of you...</p>
+        <p>• Sing a hymn (consider repeating some hymns so they can learn them, but variety is good)</p>
+        <p>• Prayer - consider a theme (thankfulness from today, mom's health, something God has done), each child has a turn praying, even the youngest.</p>
+        <p>• Pray - "Spirit, help us to understand your Word, and write it on our hearts"</p>
+        <p>• Bible reading - read through a Gospel, one section each night</p>
+        <p>• Ask - What did you hear in the Scripture reading?</p>
+        <p>• Closing prayer</p>
+
+        <hr>
+
+        <h3>Other good things</h3>
+        <p>• Music videos - check out this <a class="callout alt" href="https://www.youtube.com/playlist?list=PLPAC0JCGMntT5YT98E1nAp5aVDTDz8_lc">YouTube playlist</a></p>
+        <p>• Read books together aloud</p>
       </div>
     </div>
   </div>
